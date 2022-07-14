f2ee1adf2aad2809bd630c6a5a29a525c3ccef1b
deepanshu malviya <mdeepanshu143@gmail.com>
2022-07-14 21:35:07 +0530

    added content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
-import Hero from './components/hero/Hero';
+import "./App.css";
+import Hero from "./components/hero/Hero";
+import Plans from "./components/plans/Plans";
+import Programs from "./components/programs/Programs";
+import Reasons from "./components/reasons/Reasons";
+
 function App() {
   return (
     <div className="App">
-      <Hero/>
+      <Hero />
+      <Programs />
+      <Reasons/>
+      <Plans/>
     </div>
   );
 }
